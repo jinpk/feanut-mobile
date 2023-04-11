ff94999a077fc01eb68eb104f0b4ea5eb00a609c
Jin Park <jinpk@users.noreply.github.com>
2023-04-11 22:33:56 +0900

    fix: Fix cdn url reference

--- src/libs/common/configs.ts
+++ src/libs/common/configs.ts
@@ -3,7 +3,6 @@ const WEBSITE_URL = 'https://feanut.com';
 let apiBaseURL = 'https://api.feanut.com';
 if (__DEV__) {
   apiBaseURL = 'https://api.dev.feanut.com';
-  // apiBaseURL = 'http://192.168.35.205:3000';
 } else {
   apiBaseURL = 'https://api.dev.feanut.com';
 }
@@ -24,8 +23,7 @@ export const configs = {
   instagramClientId: '201156389218583',
 
   apiBaseURL,
-
-  cdnBaseUrl: __DEV__ ? 'https://cdn.dev.feanut.com' : 'https://cdn.feanut.com',
+  cdnBaseUrl,
   assetBaseUrl,
   verionUrl,
 
